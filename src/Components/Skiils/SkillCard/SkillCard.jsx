fccaad85e771e1175891f7dfcc11ce08e5d62301
zaidnizam <138511600+zaidnizam@users.noreply.github.com>
2024-01-08 20:37:33 +0530

    Add files via upload

---
+++ src/Components/Skiils/SkillCard/SkillCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Sk.css';
+
+const SkillCard = ({ title, iconUrl, isActive, onClick }) => {
+  return (
+    <div
+    className={`skills-card ${isActive ? "active" : ""}`}
+    onClick={() => onClick()}
+    >
+
+      <div className="skill-icon">
+        <img src={iconUrl} alt={title} />
+      </div>
+
+      <span>{title}</span>
+      
+    </div>
+  )
+}
+
+export default SkillCard;
---
+++ src/Components/SpeedInsights/SpeedInsights.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+const SpeedInsights = () => {
+  return (
+    <div>SpeedInsights</div>
+  )
+}
+
+export default SpeedInsights;
