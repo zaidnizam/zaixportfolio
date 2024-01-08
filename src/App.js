41414550c06015987a3e4d710232542e5c734039
zaidnizam <138511600+zaidnizam@users.noreply.github.com>
2024-01-08 17:22:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import Skills from './Components/Skiils/Skills';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className='container'>
+        <Hero />
+        <Skills />
+      </div>
+
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Hero.css';
+import Logo from '../Assets/Logo.png';
+import ReactLogo from '../Assets/React-Logo.png';
+import JavascriptLogo from '../Assets/Javascript-Logo.png';
+import HtmlLogo from '../Assets/Html-Logo.png';
+import CssLogo from '../Assets/Css-Logo.png';
+
+
+const Hero = () => {
+  return (
+    <section className='hero-container'>
+      <div className="hero-content">
+        <h2>Building Digital Experiences That Inspire</h2>
+        <p>
+          I am a front-end developer, creating stunning websites and crafting engaging user experiences.
+        </p>
+      </div>
+
+      <div className="hero-img">
+        <div>
+          <div className='tech-icon'>
+            <img src={ReactLogo} alt="React Image" />
+          </div>
+          <img src={Logo} alt="Background Image" />
+        </div>
+
+
+        <div>
+          <div className="tech-icon">
+            <img src={HtmlLogo} alt="Html Image" />
+          </div>
+          <div className="tech-icon">
+            <img src={CssLogo} alt="Css Image" />
+          </div>
+          <div className="tech-icon">
+            <img src={JavascriptLogo} alt="Javascript Image" />
+          </div>
+        </div>
+
+
+
+
+      </div>
+
+    </section>
+  );
+};
+
+export default Hero;
