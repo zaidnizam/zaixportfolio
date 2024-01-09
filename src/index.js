d2a7be0f83ceb16be9f1b21f7144837c9c95db9f
zaidnizam <138511600+zaidnizam@users.noreply.github.com>
2024-01-09 12:25:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "./slick-carousel/slick/slick.css";
+import "./slick-carousel/slick/slick-theme.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
